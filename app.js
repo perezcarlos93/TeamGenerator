0ad43caa2fc173fe90672cf307bedaac1ed00c63
perezcarlos93 <perez.carlos_a@yahoo.com>
2020-12-04 16:16:49 -0800

    refactored question and main inquirer function to recieve and run other inquirer functions depending on the chosen role. These additional role functions are currently not working and returning an unknown object automatically, indicating that something is being set as the answer prior to user having the opportunity to answer themselves

--- app.js
+++ app.js
@@ -14,62 +14,111 @@ const render = require("./lib/htmlRenderer");
 var array = [];
 var amount;
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-const collectAnswers = async (inputs = []) => {
-    
-    const {finalize, ...answers} = await inquirer.prompt(question)
-    const newInputs = [...inputs, answers]
-    console.log(newInputs);
-    if(answers.roleManager === true){
-        Man = new Manager(answers.name, "Manager", answers.id, answers.email, answers.number);
-        inputs.push(Man);
-        console.log("New Manager added: " + Man)
-        return Man
-    }else if(answers.roleEngineer === true){
-        Eng = new Engineer(answers.name, "Engineer", answers.id, answers.email, answers.github);
-        inputs.push(Eng);
-        console.log("New Engineer added: " + Eng)
-        return Eng
-    }else if (answers.roleIntern === true){
-        Int = new Intern(answers.name, "Intern", answers.id, answers.email, answers.school);
-        inputs.push(Int);
-        console.log("New Intern added: " + JSON.stringify(Int))
-        return Int
-    }
-    return finalize ? collectAnswers(newInputs): newInputs; 
-};
+const manQuestion = (answers) => {
+    var number;
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Please enter Manager's office number",
+            name: 'number',
+        },
+    ]).then((manager) => { number = manager.number; return number})
+    .catch(error => {
+        if(error.isTtyError) {
+          console.log(error)
+        } else {
+            // Something else when wrong
+            console.log(error)
+        }
+      });
 
-const run = async () => {
-    const inputs = await collectAnswers();
-    console.log(inputs)
+    var Man = new Manager(answers.name, answers.role, answers.id, answers.email, number);
+    array.push(Man);
+    console.log("Current Employee Array: " + array)
 }
 
-run();
+const engQuestion = (answers) => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Please enter the Engineer's github username",
+            name: 'github',
+        }
+    ]).then((engineer) => { return github = engineer.github})
+    .catch(error => {
+        if(error.isTtyError) {
+          console.log(error)
+        } else {
+            // Something else when wrong
+            console.log(error)
+        }
+      });
 
-// inquirer.prompt(question).then(function(answers){
-//     if(answers.roleManager === true){
-//         Man = new Manager(answers.name, "Manager", answers.id, answers.email, answers.number);
-//         array.push(Man);
-//     }else if(answers.roleEngineer === true){
-//         Eng = new Engineer(answers.name, "Engineer", answers.id, answers.email, answers.github);
-//         array.push(Eng);
-//     }else if (answers.roleIntern === true){
-//         Int = new Intern(answers.name, "Intern", answers.id, answers.email, answers.school);
-//         array.push(Int);
-//     }
-//     console.log("Current team array: " + JSON.stringify(array))
-// }).catch((error) => { if(error){console.log( "An error occurred: " + error )} })
+    var Eng = new Engineer(answers.name, answers.role, answers.id, answers.email, github);
+    array.push(Eng);
+    console.log("Current Employee Array: " + array)
+}
 
+const intQuestion = (answers) => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Please enter the Intern's School",
+            name: 'school'
+        }
+    ]).then((intern) => { return school = intern.school})
+    .catch(error => {
+        if(error.isTtyError) {
+          console.log(error)
+        } else {
+            // Something else when wrong
+            console.log(error)
+        }
+      });
+
+
+    var Int = new Intern(answers.name, answers.role, answers.id, answers.email, school);
+    console.log("New Intern added: " + JSON.stringify(Int))
+    array.push(Int);
+    console.log("Current Employee Array: " + array)
+}
+
+const ask = function(){
+    inquirer.prompt(question)
+    .then((answers) => {
+        if(answers.role === "manager"){
+            manQuestion(answers)
+        }else if
+        (answers.role === "engineer"){
+            engQuestion(answers)
+        }else if
+        (answers.role === "intern"){
+            intQuestion(answers)
+        }
+        
+        // finalize(answers);
+
+        console.log("Current Employee Array: " + array)
     
+    })}
 
+
+// const finalize = function(answers){
+//     ask();
+//     if(answers.finalize){
+//         ask();
+//     }
+// }
+
+// finalize();
+ask()
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-// const roster = render(array);
+const roster = render(array);
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -87,4 +136,4 @@ run();
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// for the provided `render` function to work!
